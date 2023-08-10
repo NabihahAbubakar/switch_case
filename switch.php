40e4c4500aebbb587ab6f1b89475aaed1edf51ed
NabihahAbubakar <141896146+NabihahAbubakar@users.noreply.github.com>
2023-08-10 09:32:29 +0500

    Add files via upload

---
+++ switch.php
@@ -0,0 +1,53 @@
+<?php
+$alpha="A";
+
+switch($alpha){
+case "A":
+     case "a":   
+echo "Vovel";
+break;
+
+case "E":
+echo "Vovel";
+break;
+
+case "I" :
+echo "Vovel";
+break;
+
+case "O":
+echo "Vovel";
+break;
+
+case "U" :
+echo "Vovel";
+break;
+
+default:
+echo "consonent";
+        
+
+
+}
+?>
+
+
+<!-- case "a":
+echo "Vovel";
+break;
+
+case "e":
+echo "Vovel";
+break;
+
+case "i" :
+echo "Vovel";
+break;
+
+case "o":
+echo "Vovel";
+break;
+
+case "u" :
+echo "Vovel";
+break; -->
